22af08aeb4d417cc9d94bcb8c71371d46c00f0f6
Harrisonkamau <kamauharrison87@gmail.com>
2016-08-10 14:39:24 +0300

    add common elements question

---
+++ questions/common_element.py
@@ -0,0 +1,11 @@
+""" Create a function common_element() that takes two lists as its parameters
+    return the common elements.
+    For instance, calling common_element([1,2,3], [3,43,1]) should
+    return [1,3]
+"""
+
+"""
+Create a function num_to_words() that takes a integer as an argument and returns
+its word. E.g num_to_words(2) should print "two" and num_to_words(20) should
+return "two zero"
+"""
