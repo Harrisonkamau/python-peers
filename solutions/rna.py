b7505c1a930b1af6dea7da0030558fc4194737f9
Harrisonkamau <kamauharrison87@gmail.com>
2016-08-13 11:15:12 +0300

    add solution to rna transcription

---
+++ solutions/rna.py
@@ -0,0 +1,25 @@
+import unittest
+dna_dict = {'G': 'C','C': 'G','T': 'A','A': 'U'}
+
+
+def to_rna(dna):
+    return ''.join(dna_dict[nucleotide] for nucleotide in dna)
+
+
+class DNATests(unittest.TestCase):
+
+    def test_transcribes_guanine_to_cytosine(self):
+        self.assertEqual('C', to_rna('G'))
+
+    def test_transcribes_cytosine_to_guanine(self):
+        self.assertEqual('G', to_rna('C'))
+
+    def test_transcribes_thymine_to_adenine(self):
+        self.assertEqual('A', to_rna('T'))
+
+    def test_transcribes_adenine_to_uracil(self):
+        self.assertEqual('U', to_rna('A'))
+
+    def test_transcribes_all_occurences(self):
+        self.assertMultiLineEqual('UGCACCAGAAUU', to_rna('ACGTGGTCTTAA'))
+
