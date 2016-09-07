e26af1d119a34b310e92b1d1201258d7981c5960
Brian Lusina <lusinabrian@gmail.com>
2016-09-07 18:22:28 +0300

    Remove complementary solution from master

--- complementary_dna/complementary.py
+++ complementary_dna/complementary.py
@@ -7,9 +7,4 @@ In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G
 You have function with one side of the DNA; you need to get the other complementary side.
 DNA strand is never empty or there is no DNA at all.
 """
-strands ={"A":"T", "C":"G"}
 
-def dna_strand(dna):
-	if dna == "" or not isinstance(dna, str):
-		raise ValueError("DNA strands can not be empty or non string")
-	return "".join([strands.get(x) for x in dna.upper()])
